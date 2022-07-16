0088148e189e74c31577d969d736ca7a7b5c8f57
mauro137 <maurolizarriaga2@gmail.com>
2022-07-16 19:43:40 -0300

    main page visual completado, falta corregir imagenes svg, screen de login completado, solo le falta la media querie

---
+++ js/header_footer.js
@@ -0,0 +1,115 @@
+class MyHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = ` 
+      <header class="header">
+      <img
+        class="header__logo"
+        src="estilos/HEADER/recursos/logo_alura.svg"
+        alt="Logo alura"
+      />
+      <button class="header__btn__login">
+        <a href="/seccion-login.html" target="_blank">
+          <span class="btnLogin__txt"> Login </span></a
+        >
+      </button>
+      <div class="buscador"> 
+      <input type="text" class="buscador__input" placeholder="¿Que deseas hacer?">
+      <img
+        class="lupa__logo"
+        src="estilos/HEADER/recursos/lupa.svg"
+        alt="Lupa buscador"
+      />
+      </div>
+    </header> 
+          `;
+  }
+}
+customElements.define("my-header", MyHeader);
+
+class MyFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = ` 
+      <footer class="footer">
+      <section class="seccion__contacto">
+        <div class="footer__info__frecuente">
+          <div class="footer__contenedor__logo">
+            <img
+              class="footer__logo__alura"
+              src="estilos/HEADER/recursos/logo_alura.svg"
+              alt="logo alura"
+            />
+          </div>
+          <div class="info__frecuente__links">
+            <a
+              class="info__links-links"
+              href="http://"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Quienes somos</a
+            >
+            <a
+              class="info__links-links"
+              href="http://"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Politica de privacidad</a
+            >
+            <a
+              class="info__links-links"
+              href="http://"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Programa de fidelidad</a
+            >
+            <a
+              class="info__links-links"
+              href="http://"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Nuestras tiendas</a
+            >
+            <a
+              class="info__links-links"
+              href="http://"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Quiero ser franquiciado</a
+            >
+            <a
+              class="info__links-links"
+              href="http://"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Anuncie aqui</a
+            >
+          </div>
+        </div>
+
+        <form class="contacto__form">
+          <span class="producto__precio">Habla con nosotros</span>
+          <input
+            type="text"
+            placeholder="Nombre"
+            spellcheck="false"
+            autocomplete="off"
+          />
+          <textarea
+            name="mensaje"
+            id="mensaje"
+            cols="20"
+            rows="4"
+            placeholder="Escribe un mensaje"
+          ></textarea>
+          <button class="btn__promocion" type="submit">
+            <span class="btn__texto"> Enviar Mensaje </span>
+          </button>
+        </form>
+      </section>
+      <section class="seccion__rodapie">
+        <div class="web__autor">Desarrollado por Mauro Lizarriaga</div>
+      </section>
+    </footer>
+      `;
+  }
+}
+customElements.define("my-footer", MyFooter);
