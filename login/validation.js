2858e36ea95583c5b52ad69087c6805165801a1c
mauro137 <maurolizarriaga2@gmail.com>
2022-08-07 17:33:26 -0300

    validacion de formulario (agregar producto) finalizado

--- login/validation.js
+++ login/validation.js
@@ -1,22 +1,24 @@
 const productoForm = document.querySelector(".agregar__producto__form");
-const nombre = document.querySelector('#form__input__nombre');
-const precio = document.querySelector('#form__input__precio');
-const descripcion = document.querySelector('#producto__descripcion');
-const inputs = document.querySelectorAll(".agregar__producto__form input, textarea");
+const nombre = document.querySelector("#form__input__nombre");
+const precio = document.querySelector("#form__input__precio");
+const descripcion = document.querySelector("#producto__descripcion");
+const inputs = document.querySelectorAll(
+  ".agregar__producto__form input, textarea"
+);
 const textarea = document.querySelector(".agregar__producto");
 const btnSubmit = document.querySelector(".login__button--producto");
 
 const expresiones = {
-    nombre:  /^([A-Za-z]){1,20}$/,
-    precio: /[^a-z ]\ *([.0-9])*\d/g,
-    descripcion: /^([A-Za-z0-9!#%$+-?=*&".,]){1,150}$/,
-  };
+  nombre: /^([A-Za-z ]){1,20}$/,
+  precio: /[^a-z ]\ *([.0-9])*\d/g,
+  descripcion: /^([A-Za-z0-9!#%$+-? =*&".,]){1,150}$/,
+};
 
-  
 const campos = {
   nombre: false,
   precio: false,
   descripcion: false,
+  categoria: false,
 };
 
 const validarFormulario = (e) => {
@@ -35,7 +37,7 @@ const validarFormulario = (e) => {
 
 const validarCampo = (expresion, input, campo) => {
   let campoBox = document.getElementById(`${campo}`);
-  console.log(campoBox)
+  console.log(campoBox);
   if (expresion.test(input.value)) {
     campoBox.classList.remove("formulario__incorrecto");
     campoBox.classList.add("formulario__correcto");
@@ -56,12 +58,25 @@ inputs.forEach((input) => {
 });
 
 productoForm.addEventListener("submit", (e) => {
-e.preventDefault();
-if(campos.nombre && campos.precio && campos.descripcion) {
-  alert("Producto Agregado con exito");
-}
-else{
-  alert("Falló la carga del producto, revisar formulario");
-}
+  let msgExito = document.querySelector(".form__exito");
+  let msgError = document.querySelector(".form__error");
+  e.preventDefault();
+  if (campos.nombre && campos.precio && campos.descripcion) {
+    msgExito.classList.add("form__exito--mostrar");
 
+    setTimeout(() => {
+      msgExito.classList.remove("form__exito--mostrar");
+    }, 5000);
+    productoForm.reset();
+    document
+      .querySelectorAll(".formulario__correcto")
+      .forEach((campo_valido) => {
+        campo_valido.classList.remove("formulario__correcto");
+      });
+  } else {
+   msgError.classList.add('form__error--mostrar');
+   setTimeout(() => {
+    msgError.classList.remove("form__error--mostrar");
+  }, 5000);
+  }
 });
